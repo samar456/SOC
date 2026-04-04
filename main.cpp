87f4ddf48afd3bb491b3662ce5f00ca0a0666551
Samar <samar@Samars-MacBook-Air.local>
2026-04-05 03:11:45 +0530

    Initial commit - main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <systemc>
+
+using namespace sc_core;
+
+SC_MODULE(Scaler)
+{
+    SC_CTOR(Scaler)
+    {
+        SC_THREAD(do_operation);
+        sc_time_stamp();
+    }
+
+    void do_operation()
+    {
+        std::cout << "Hello SystemC! Time = "
+                  << sc_time_stamp() << std::endl;
+        sc_stop();
+        wait(10, SC_NS);
+    
+        std::cout << "Finished at "
+                  << sc_time_stamp() << std::endl;
+    }
+};
+
+int sc_main(int argc, char* argv[])
+{
+    Scaler s("s");
+    sc_start();
+    return 0;
+}
